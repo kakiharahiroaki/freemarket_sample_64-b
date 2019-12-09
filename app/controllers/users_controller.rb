861d9dd94ee0ed7ea6971875acd9e3e5568da2e7
shogo-naga8 <x08smjkcrXxx@gmail.com>
2019-12-09 20:07:36 +0900

    ユーザープロフィール編集ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def updata
+  def update
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  root  'items#index'
-  resources :items, only: [:index]
-  resources :users do
-    collection do
-
-      get 'update'
-
-    end
-  end
+  root  'users#edit'
+  
+  
 end
