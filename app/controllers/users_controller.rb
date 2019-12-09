da6299849c5ed8c2342f1153c829234dab80a706
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-09 10:43:46 +0900

    userscontroller作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
-
 class UsersController <  ApplicationController
    
-    def destroy
-    end
+  def destroy
+  end
 
+  def update
+  end
 
-    def update
-    end
+  def index
+  end
 
-  
+  def registration
+  end
+
+  def sms_confirmation
+  end
+
+  def input_address
+  end
+
+  def payment
+  end
+
+  def login
+  end
 
 end
