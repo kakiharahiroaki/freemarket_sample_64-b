da26b78c3cacd7fd6c4c7c814280912c65be688b
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-09 10:47:38 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root  'items#index'
+
+
+root  'items#index'
   get  "/buys/index", to: "buys#index"
   resources :items, only: [:index]
+ root  'top#index'
+ resources :users do
+    collection do
+      get 'index'
+      get 'registration'
+      get 'sms_confirmation'
+      get 'input_address'
+      get 'payment'
+      get 'signup_complete'
+      get 'login'
+    end
+  end
 end
 
 
