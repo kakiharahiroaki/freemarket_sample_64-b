b9590c397f7f036fa4450f726a5ec17c84ab77f6
shogo-naga8 <x08smjkcrXxx@gmail.com>
2019-12-14 16:38:50 +0900

    修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,20 +10,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else  #もし@userがDBにいない場合、新規登録ページにリダイレクトします
       session["devise.facebook_data"] = request.env["omniauth.auth"]
         #データをsessionに入れることによって、新規登録ページの入力欄に、予め情報を入れておくなどが可能になります。
-      redirect_to 新規登録ページ
+      redirect_to new_menber_users_path
     end
   end
 
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication 
       set_flash_message(:notice, :success, kind: 'google') if is_navigational_format?
     else
       session["devise.google_data"] = request.env["omniauth.auth"][:info]
       #google認証の場合は、なぜかauth_hashの容量が大きく、一瞬で容量オーバーとなるため、新規登録時に必要な情報のみをsessionに渡すこととしました。（おそらく画像データのせい？）
-      redirect_to 新規登録ページ
+      redirect_to new_menber_users_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-
          :recoverable, :rememberable, :validatable
   has_one :address
   accepts_nested_attributes_for :address
-
-         :recoverable, :rememberable, :validatable,:omniauthable
-
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i[facebook google_oauth2]
+  has_many :sns_credentials, dependent: :destroy
 
       def self.from_omniauth(auth)
       user = User.where(email: auth.info.email).first
@@ -16,6 +14,8 @@ class User < ApplicationRecord
       if user.present?
         unless sns_credential_record.present?
           SnsCredential.create(
+            users: auth.users,
+            token: auth.token,
             user_id: user.id,
             provider: auth.provider,
             uid: auth.uid
@@ -26,14 +26,16 @@ class User < ApplicationRecord
           id: User.all.last.id + 1,
           email: auth.info.email,
           password: Devise.friendly_token[0, 20],
-          nickname: auth.info.name,
-          last_name: auth.info.last_name,
-          first_name: auth.info.first_name,
+          # nickname: auth.info.name,
+          # last_name: auth.info.last_name,
+          # first_name: auth.info.first_name,
         )
         SnsCredential.new(
+          users: auth.users,
+          token: auth.token,
           provider: auth.provider,
           uid: auth.uid,
-          user_id: user.id
+          # user_id: user.id
         )
       end 
     user
