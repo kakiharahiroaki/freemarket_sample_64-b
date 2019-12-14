4130d916d675fa5d85b0386a0ff70478208fd8e4
bareruw <bkgw0413@gmail.com>
2019-12-14 17:28:46 +0900

    sessionの追加

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -7,12 +7,37 @@ class SignupController < ApplicationController
   end
 
   def sms_confirmation
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:family_name_kanji] = user_params[:family_name_kanji]
+    session[:first_name_kanji] = user_params[:first_name_kanji]
+    session[:family_name_kana] = user_params[:family_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    session[:birthdata_year] = user_params[:birthdata_year]
+    session[:birthdata_month] = user_params[:birthdata_month]
+    session[:birthdata_day] = user_params[:birthdata_day]
+    @user = User.new
+    
   end
 
   def input_address
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
+    
   end
 
   def payment
+    session[:family_name_kanji] = user_params[:family_name_kanji]
+    session[:first_name_kanji] = user_params[:first_name_kanji]
+    session[:family_name_kana] = user_params[:family_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    session[:postcode] = user_params[:postcode]
+    session[:prefecture] = user_params[:prefecture]
+    session[:city] = user_params[:city]
+    session[:block] = user_params[:block]
+    session[:building] = user_params[:building]
+    @user = User.new
   end
 
   def signup_complete
@@ -34,7 +59,7 @@ class SignupController < ApplicationController
       :birthdata_day,
       :phone_number,
       :email,
-      :encrypted_password,
+      :password,
       :reset_password_token,
       :reset_password_sent_at,
       :remember_created_at,
