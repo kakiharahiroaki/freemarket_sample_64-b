3a2d6b4ee456324c8ea989f27779b16c1c030354
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-16 16:25:44 +0900

    users_view関連修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,18 @@ Rails.application.routes.draw do
   get "/items/show", to: "items#show"
   get  "/buys/index", to: "buys#index"
   get "/sells/index", to: "sells#index"
-  get  "/users/show", to: "users#show"
+  get "/cards/index"
 
   resources :items, only: [:index]
+
+  resources :users do
+    collection do
+      get 'create'
+      get 'destroy'
+      get 'show'
+      get 'update'
+    end
+  end
   resources :signup do
     collection do
       get 'index'
