f253f0633d9d8484851e8f5a2361308c76421be0
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-18 20:12:11 +0900

    itemsマイグレーションファイルの編集

--- db/migrate/20191218104447_create_items.rb
+++ db/migrate/20191218104447_create_items.rb
@@ -1,7 +1,20 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :products null: false
+      t.string :description_of_item, null: false
+      t.integer :price, null: false
+      t.integer :size, null: false
+      t.integer :shipping_date, null:false
+      t.string :shipping_method, null:false
+      t.integer :postage, null:false
+      t.string :shipping_origin null: false
+      t.string :condition, null: false
+      t.integer :brand_id, null: false, foreign_key: true
+      t.integer :category_id, null:false, foreign_key: true
+      t.integer :user_id, null:false, foreign_key: true
+      t.integer :saler_id, null:false, foreign_key: true
+      t.integer :buyer_id, null:false, foreign_key: true
       t.timestamps
     end
   end
