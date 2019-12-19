a0a5ad8b14711a0854cd89a9b97a5bcfa0695e1c
melaleyraud6 <theflare.akg.9pb@gmail.com>
2019-12-19 16:49:46 +0900

    手数料計算
    販売価格に半角数字で入力があったら、手数料と販売利益を自動で計算する

---
+++ app/assets/javascripts/sells_fee.js
@@ -0,0 +1,28 @@
+$(function() {
+  $("#sells__price").on("input", function(e) {
+    e.preventDefault()
+    let input = $("#sells__price").val();
+    
+    
+    console.log(input);
+    if ((input >= 300 ) && (input <= 9999999 )) {
+      let fee = Math.floor(input*0.1);
+      console.log(fee);
+      let profit = (input - fee);
+      $(".sells__right1").html(fee);
+      $(".sells__right1").prepend("¥");
+      $(".sells__right2").html(profit);
+      $(".sells__right2").prepend("¥");
+      $("#price").val(input);
+    }
+    else if (input <= 300 || input >= 9999999){
+      $(".sells__right1").html("-");
+      $(".sells__right2").html("-");
+    }
+    else if (input == "") {
+        $(".sells__right1").html("-");
+        $(".sells__right2").html("-");
+    }
+  });
+
+});
