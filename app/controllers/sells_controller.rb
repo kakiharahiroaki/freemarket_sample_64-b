f63a88eeea25e8feb0619c4c944c545c382169bb
melaleyraud6 <theflare.akg.9pb@gmail.com>
2019-12-19 18:59:15 +0900

    Update sells_controller.rb

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -3,31 +3,31 @@ class SellsController < ApplicationController
 
 
    def index
-    # @item = Item.find(params[:id])
-    # gon.item = @item
-    # gon.item_images = @item.item_images
+    @item = Item.find(params[:id])
+    gon.item = @item
+    gon.item_images = @item.item_images
 
-    # # @item.item_imagse.image_urlをバイナリーデータにしてビューで表示できるようにする
-    # require 'base64'
-    # require 'aws-sdk'
+    # @item.item_imagse.image_urlをバイナリーデータにしてビューで表示できるようにする
+    require 'base64'
+    require 'aws-sdk'
 
-    # gon.item_images_binary_datas = []
-    # if Rails.env.production?
-    #   client = Aws::S3::Client.new(
-    #                          region: 'ap-northeast-1',
-    #                          access_key_id: Rails.application.credentials.aws[:access_key_id],
-    #                          secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    #                          )
-    #   @item.item_images.each do |image|
-    #     binary_data = client.get_object(bucket: 'freemarket-sample-51a', key: image.image_url.file.path).body.read
-    #     gon.item_images_binary_datas << Base64.strict_encode64(binary_data)
-    #   end
-    # else
-    #   @item.item_images.each do |image|
-    #     binary_data = File.read(image.image_url.file.file)
-    #     gon.item_images_binary_datas << Base64.strict_encode64(binary_data)
-    #   end
-    # end
+    gon.item_images_binary_datas = []
+    if Rails.env.production?
+      client = Aws::S3::Client.new(
+                             region: 'ap-northeast-1',
+                             access_key_id: Rails.application.credentials.aws[:access_key_id],
+                             secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+                             )
+      @item.item_images.each do |image|
+        binary_data = client.get_object(bucket: 'freemarket-sample-51a', key: image.image_url.file.path).body.read
+        gon.item_images_binary_datas << Base64.strict_encode64(binary_data)
+      end
+    else
+      @item.item_images.each do |image|
+        binary_data = File.read(image.image_url.file.file)
+        gon.item_images_binary_datas << Base64.strict_encode64(binary_data)
+      end
+    end
   end
 
   def update
