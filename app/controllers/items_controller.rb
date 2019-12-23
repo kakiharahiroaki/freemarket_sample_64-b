a1a1dd4383a4b24fac54419ac83d02199e9c84a8
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-23 19:50:01 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,9 @@ class ItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:id])
-      if @item.user_id == current_user.id
-        @item.destroy #destroyメソッドを使用し対象のツイートを削除する。
+    if @item.user_id == current_user.id
+      @item.destroy #destroyメソッドを使用し対象のツイートを削除する。
       redirect_to root_path
-     end
-   end
+    end
+  end
 end
