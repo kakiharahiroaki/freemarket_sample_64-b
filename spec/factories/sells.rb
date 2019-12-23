027337d21f322b1519459e2ee706597f18fcaa0e
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-23 13:50:42 +0900

    haml修正

---
+++ spec/factories/sells.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :product do
+
+id                    {1}
+user_id               {4}
+products              {"シャツ"}
+description_of_item   {"返品受け付けません"}
+price                 {"3000"}
+size                  {12}
+shipping_date         {1}
+shipping_method       {2}
+postage               {1}
+shipping_origin       {3}
+condition             {1}
+  
+  end
+end
