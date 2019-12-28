3ea6006eed52803d824887de1deb977100370896
melaleyraud6 <theflare.akg.9pb@gmail.com>
2019-12-28 21:51:33 +0900

    ステータスedit

--- db/migrate/20191218112632_create_statuses.rb
+++
@@ -1,9 +0,0 @@
-class CreateStatuses < ActiveRecord::Migration[5.2]
-  def change
-    create_table :statuses do |t|
-      t.integer :status, null: false
-      t.integer :item_id, null: false
-      t.timestamps
-    end
-  end
-end
