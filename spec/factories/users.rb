4f0c56d2313079c5fc1697186ae8cd6f739741d9
melaleyraud6 <theflare.akg.9pb@gmail.com>
2020-01-03 14:44:35 +0900

    商品情報編集単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
 
   factory :user do
+    id                    {4}
     nickname              {"山田"}
     email                 {"kkk@gmail.com"}
     password              {"yyy00000"}
@@ -12,6 +13,8 @@ FactoryBot.define do
     birthdata_month       {"12"}
     birthdata_day         {"18"}
     phone_number          {"08056784321"}
+    created_at            {"2019-12-30 21:47:58"}
+    updated_at            {"2019-12-30 21:47:58"}
   end
 
 end
